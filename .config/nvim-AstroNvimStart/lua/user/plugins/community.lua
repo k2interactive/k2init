eacef454bd18f18b075be4bfcb598567c943f2e9
k2 <k2interactivestudios@gmail.com>
2023-09-24 01:38:00 -0700

    feat: Add Diffview-nvim to astro

--- .config/nvim-AstroNvimStart/lua/user/plugins/community.lua
+++ .config/nvim-AstroNvimStart/lua/user/plugins/community.lua
@@ -5,4 +5,5 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.git.diffview-nvim"},
 }
